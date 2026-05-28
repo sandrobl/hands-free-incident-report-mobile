767d9769a6d64318ef77ffdad14d2846049c8bab
sandro blatter <sandro.blatter@hotmail.com>
2026-05-28 17:14:10 +0200

    feat: Update navigation dependencies and add launcher icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.play.services.location)
+    implementation(libs.navigation.fragment.ktx)
+    implementation(libs.navigation.ui.ktx)
     testImplementation(libs.junit)
 
     implementation(libs.auth0)
